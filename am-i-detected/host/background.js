40d6325d6f13a95debea19ee13f2c3e598a9824f
Lua <140775902+liablelua@users.noreply.github.com>
2024-09-01 20:48:49 -0500

    AID --> v1.0

---
+++ am-i-detected/host/background.js
@@ -0,0 +1,28 @@
+chrome.webNavigation.onCompleted.addListener((details) => {
+    if (details.url.includes('roblox.com')) {
+      fetch("https://usermoderation.roblox.com/v1/not-approved")
+        .then(response => response.json())
+        .then(data => {
+          var Data = JSON.stringify(data, null, 2);
+          const _1 = Data.indexOf("Authentication cookie is empty");
+          const _2 = Data.indexOf("{}");
+          const _3 = Data.indexOf("true");
+          if (_1 != -1) {
+            fetch("http://localhost:8080/?detected=none");
+          } else {
+            if (_2 != -1) {
+                fetch("http://localhost:8080/?detected=false");
+            } else {
+                if (_3 != -1) {
+                    fetch("http://localhost:8080/?detected=true");
+                } else {
+                    fetch("http://localhost:8080/?detected=true");
+                }
+            }
+          }
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error);
+        });
+    }
+  }, {url: [{urlMatches: '.*roblox.com.*'}]});
