ffca4aeee679b83b6d153491184d8e97cfd80640
Lua <140775902+liablelua@users.noreply.github.com>
2024-09-01 23:22:59 -0500

    Update background.js

--- am-i-detected/host/background.js
+++ am-i-detected/host/background.js
@@ -1,37 +1,37 @@
 chrome.webNavigation.onCompleted.addListener((details) => {
-    if (details.url.includes('roblox.com')) {
-      fetch("https://users.roblox.com/v1/users/authenticated").then(response => response.json).then(data => {
-        var username = data.name;
-        var display = data.displayName;
-        if (username == display) {
-            fetch("http://localhost:8080/?username=" + username);
+  if (details.url.includes('roblox.com')) {
+    fetch("https://users.roblox.com/v1/users/authenticated").then(response => response.json()).then(data => {
+      var username = data.name;
+      var display = data.displayName;
+      if (username == display) {
+          fetch("http://localhost:8080/?username=" + username);
+      } else {
+          fetch("http://localhost:8080/?username=" + display + " (@" + username + ")");
+      }
+    })
+    fetch("https://usermoderation.roblox.com/v1/not-approved")
+      .then(response => response.json())
+      .then(data => {
+        var Data = JSON.stringify(data, null, 2);
+        const _1 = Data.indexOf("Authentication cookie is empty");
+        const _2 = Data.indexOf("{}");
+        const _3 = Data.indexOf("true");
+        if (_1 != -1) {
+          fetch("http://localhost:8080/?detected=none");
         } else {
-            fetch("http://localhost:8080/?username=" + display + " (@" + username + ")");
-        }
-      });
-      fetch("https://usermoderation.roblox.com/v1/not-approved")
-        .then(response => response.json())
-        .then(data => {
-          var Data = JSON.stringify(data, null, 2);
-          const _1 = Data.indexOf("Authentication cookie is empty");
-          const _2 = Data.indexOf("{}");
-          const _3 = Data.indexOf("true");
-          if (_1 != -1) {
-            fetch("http://localhost:8080/?detected=none");
+          if (_2 != -1) {
+              fetch("http://localhost:8080/?detected=false");
           } else {
-            if (_2 != -1) {
-                fetch("http://localhost:8080/?detected=false");
-            } else {
-                if (_3 != -1) {
-                    fetch("http://localhost:8080/?detected=true");
-                } else {
-                    fetch("http://localhost:8080/?detected=true");
-                }
-            }
+              if (_3 != -1) {
+                  fetch("http://localhost:8080/?detected=true");
+              } else {
+                  fetch("http://localhost:8080/?detected=true");
+              }
           }
-        })
-        .catch(error => {
-          console.error('Error fetching data:', error);
-        });
-    }
-  }, {url: [{urlMatches: '.*roblox.com.*'}]});
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }
+}, {url: [{urlMatches: '.*roblox.com.*'}]});
